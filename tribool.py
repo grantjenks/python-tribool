46e34fe7ec1db7683f6dbae35238a58b8c942f2b
Grant Jenks <contact@grantjenks.com>
2016-04-02 11:08:09 -0700

    Bump version to 0.7.0

--- tribool.py
+++ tribool.py
@@ -221,8 +221,8 @@ class Tribool(tuple):
 
 
 __title__ = 'tribool'
-__version__ = '0.6.2'
-__build__ = 0x000602
+__version__ = '0.7.0'
+__build__ = 0x000700
 __author__ = 'Grant Jenks'
 __license__ = 'Apache 2.0'
 __copyright__ = 'Copyright 2016 Grant Jenks'
